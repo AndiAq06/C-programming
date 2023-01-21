8575659b6f4b3551e05bcbe89e4c489a0d48f6f1
AndiAq06 <ardiansyahnasir56@gmail.com>
2023-01-21 19:55:32 +0800

    Membuat kode di file compare-to-string

--- compare-to-string.c
+++ compare-to-string.c
@@ -1,9 +1,25 @@
 #include <stdio.h>
+#include <string.h>
+#define MAX_SIZE 100
 
 
 int main(){
-    printf("Masukkan nama: ");
-    char nama[20];
-    gets(nama);
+        char str1[MAX_SIZE],str2[MAX_SIZE];
+    int i,j;
 
+    printf("Enter first string: ");
+    gets(str1);
+    printf("Enter second string: ");
+    gets(str2);
+
+    int res = strcmp(str1,str2);
+    if(res == 0){
+        printf("Both strings are equal");
+    } else if (res == -1){
+        printf("First string is smaller than second");
+    } else {
+        printf("First string greater than second string");
+    }
+
+    return 0;
 }
